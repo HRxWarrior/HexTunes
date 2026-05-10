db8c5bfc85a1e4a9209b1c2087c3ebd9c9e81378
HRxWarrior <kinghr146@gmail.com>
2026-05-10 15:03:09 +0530

    fix: compile errors in duration_ext.dart

--- lib/core/utils/duration_ext.dart
+++ lib/core/utils/duration_ext.dart
@@ -2,10 +2,14 @@ extension DurationFormat on Duration {
   String get mmss {
     final m = inMinutes.remainder(60).toString().padLeft(2, '0');
     final s = inSeconds.remainder(60).toString().padLeft(2, '0');
-    return '\$m:\$s';
+    return '$m:$s';
   }
+
   String get hmmss {
     if (inHours < 1) return mmss;
-    return '\${inHours}:\${inMinutes.remainder(60).toString().padLeft(2,'0')}:\${inSeconds.remainder(60).toString().padLeft(2,'0')}';
+    final h = inHours;
+    final m = inMinutes.remainder(60).toString().padLeft(2, '0');
+    final s = inSeconds.remainder(60).toString().padLeft(2, '0');
+    return '$h:$m:$s';
   }
 }
