e535dd3ebb75f27789129f847f4fc64a4c7a3b08
HRxWarrior <kinghr146@gmail.com>
2026-05-10 15:03:13 +0530

    fix: compile errors in app_router.dart

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/home/home_screen.dart';
@@ -23,13 +24,17 @@ final appRouterProvider = Provider<GoRouter>((ref) => GoRouter(
       builder: (ctx, state, child) => HexShell(child: child),
       routes: [
         GoRoute(path: '/home',
-          pageBuilder: (c,s) => const NoTransitionPage(child: HomeScreen())),
+          pageBuilder: (c, s) =>
+              const NoTransitionPage(child: HomeScreen())),
         GoRoute(path: '/search',
-          pageBuilder: (c,s) => const NoTransitionPage(child: SearchScreen())),
+          pageBuilder: (c, s) =>
+              const NoTransitionPage(child: SearchScreen())),
         GoRoute(path: '/library',
-          pageBuilder: (c,s) => const NoTransitionPage(child: LibraryScreen())),
+          pageBuilder: (c, s) =>
+              const NoTransitionPage(child: LibraryScreen())),
         GoRoute(path: '/settings',
-          pageBuilder: (c,s) => const NoTransitionPage(child: SettingsScreen())),
+          pageBuilder: (c, s) =>
+              const NoTransitionPage(child: SettingsScreen())),
       ],
     ),
     GoRoute(
@@ -39,36 +44,44 @@ final appRouterProvider = Provider<GoRouter>((ref) => GoRouter(
         transitionsBuilder: (ctx, anim, _, child) => SlideTransition(
           position: Tween<Offset>(
                   begin: const Offset(0, 1), end: Offset.zero)
-              .chain(CurveTween(curve: Curves.easeOutCubic)).animate(anim),
-          child: child)),
+              .chain(CurveTween(curve: Curves.easeOutCubic))
+              .animate(anim),
+          child: child,
+        ),
+      ),
     ),
     GoRoute(
       path: '/playlist/:id',
       pageBuilder: (ctx, state) => MaterialPage(
         child: PlaylistScreen(
-            playlistId: state.pathParameters['id'] ?? '')),
+            playlistId: state.pathParameters['id'] ?? ''),
+      ),
     ),
     GoRoute(
       path: '/artist/:id',
       pageBuilder: (ctx, state) {
         final extra = state.extra as Map<String, dynamic>? ?? {};
-        return MaterialPage(child: ArtistScreen(
-          artistId:   state.pathParameters['id'] ?? '',
-          artistName: extra['artistName'] as String?,
-          coverUrl:   extra['coverUrl']   as String?,
-        ));
+        return MaterialPage(
+          child: ArtistScreen(
+            artistId:   state.pathParameters['id'] ?? '',
+            artistName: extra['artistName'] as String?,
+            coverUrl:   extra['coverUrl']   as String?,
+          ),
+        );
       },
     ),
     GoRoute(
       path: '/album/:id',
       pageBuilder: (ctx, state) {
         final extra = state.extra as Map<String, dynamic>? ?? {};
-        return MaterialPage(child: AlbumScreen(
-          albumId:    state.pathParameters['id'] ?? '',
-          albumName:  extra['albumName']  as String? ?? 'Album',
-          artworkUrl: extra['artworkUrl'] as String?,
-          artistName: extra['artistName'] as String?,
-        ));
+        return MaterialPage(
+          child: AlbumScreen(
+            albumId:    state.pathParameters['id'] ?? '',
+            albumName:  extra['albumName']  as String? ?? 'Album',
+            artworkUrl: extra['artworkUrl'] as String?,
+            artistName: extra['artistName'] as String?,
+          ),
+        );
       },
     ),
   ],
