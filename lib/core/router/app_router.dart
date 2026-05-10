ede081a1d392d4c30ef6895ec3371baf3b08b99b
HRxWarrior <kinghr146@gmail.com>
2026-05-10 11:33:24 +0530

    feat: add/update app_router.dart

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -5,18 +5,31 @@ import '../../features/search/search_screen.dart';
 import '../../features/library/library_screen.dart';
 import '../../features/settings/settings_screen.dart';
 import '../../features/player/player_screen.dart';
+import '../../features/splash/splash_screen.dart';
+import '../../features/playlist/playlist_screen.dart';
+import '../../features/artist/artist_screen.dart';
+import '../../features/album/album_screen.dart';
 import '../../widgets/hex_shell.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) => GoRouter(
-  initialLocation: '/home',
+  initialLocation: '/splash',
   routes: [
+    GoRoute(
+      path: '/splash',
+      pageBuilder: (ctx, state) =>
+          const NoTransitionPage(child: SplashScreen()),
+    ),
     ShellRoute(
       builder: (ctx, state, child) => HexShell(child: child),
       routes: [
-        GoRoute(path: '/home',     pageBuilder: (c,s) => const NoTransitionPage(child: HomeScreen())),
-        GoRoute(path: '/search',   pageBuilder: (c,s) => const NoTransitionPage(child: SearchScreen())),
-        GoRoute(path: '/library',  pageBuilder: (c,s) => const NoTransitionPage(child: LibraryScreen())),
-        GoRoute(path: '/settings', pageBuilder: (c,s) => const NoTransitionPage(child: SettingsScreen())),
+        GoRoute(path: '/home',
+          pageBuilder: (c,s) => const NoTransitionPage(child: HomeScreen())),
+        GoRoute(path: '/search',
+          pageBuilder: (c,s) => const NoTransitionPage(child: SearchScreen())),
+        GoRoute(path: '/library',
+          pageBuilder: (c,s) => const NoTransitionPage(child: LibraryScreen())),
+        GoRoute(path: '/settings',
+          pageBuilder: (c,s) => const NoTransitionPage(child: SettingsScreen())),
       ],
     ),
     GoRoute(
@@ -24,10 +37,39 @@ final appRouterProvider = Provider<GoRouter>((ref) => GoRouter(
       pageBuilder: (ctx, state) => CustomTransitionPage(
         child: const PlayerScreen(),
         transitionsBuilder: (ctx, anim, _, child) => SlideTransition(
-          position: Tween<Offset>(begin: const Offset(0, 1), end: Offset.zero)
+          position: Tween<Offset>(
+                  begin: const Offset(0, 1), end: Offset.zero)
               .chain(CurveTween(curve: Curves.easeOutCubic)).animate(anim),
-          child: child),
-      ),
+          child: child)),
+    ),
+    GoRoute(
+      path: '/playlist/:id',
+      pageBuilder: (ctx, state) => MaterialPage(
+        child: PlaylistScreen(
+            playlistId: state.pathParameters['id'] ?? '')),
+    ),
+    GoRoute(
+      path: '/artist/:id',
+      pageBuilder: (ctx, state) {
+        final extra = state.extra as Map<String, dynamic>? ?? {};
+        return MaterialPage(child: ArtistScreen(
+          artistId:   state.pathParameters['id'] ?? '',
+          artistName: extra['artistName'] as String?,
+          coverUrl:   extra['coverUrl']   as String?,
+        ));
+      },
+    ),
+    GoRoute(
+      path: '/album/:id',
+      pageBuilder: (ctx, state) {
+        final extra = state.extra as Map<String, dynamic>? ?? {};
+        return MaterialPage(child: AlbumScreen(
+          albumId:    state.pathParameters['id'] ?? '',
+          albumName:  extra['albumName']  as String? ?? 'Album',
+          artworkUrl: extra['artworkUrl'] as String?,
+          artistName: extra['artistName'] as String?,
+        ));
+      },
     ),
   ],
 ));
